d55d62f51c23b2964b2abdc8d1e70550ca89bd4a
anrajin <an.rajin@gmail.com>
2022-05-13 11:51:04 +0600

    Prototype design pattern

--- CreationalPattern/CreationalPattern/Program.cs
+++ CreationalPattern/CreationalPattern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CreationalPattern.Singleton;
+using CreationalPattern.Prototype;
 
 
 /*Singleton*/
@@ -7,3 +8,15 @@ fromFather.Write("Father instantiated");
 
 Logger fromMother = Logger.GetLogger();
 fromMother.Write("Mother instantiated");
+
+
+/*Prototype*/
+Zoombie zoombie = new Zoombie();
+zoombie.Look = "Red";
+zoombie.Health = 100;
+zoombie.Strength = 200;
+zoombie.Speed = 300;
+
+//clone the object form the prototype
+var zoombie1 = zoombie.Copy();
+Console.WriteLine(zoombie1.Look);
---
+++ CreationalPattern/CreationalPattern/Prototype/Readme.md
@@ -0,0 +1,6 @@
+﻿Prototype Design Pattern specifies the kind of objects to create using a prototypical 
+instance, and create new objects by copying this prototype
+
+Prototype Design Pattern gives us a way to create new objects from the existing instance of the object. 
+That means it clone the existing object with its data into a new object. If we do any changes to the cloned 
+object (i.e. new object) then it does not affect the original object.
